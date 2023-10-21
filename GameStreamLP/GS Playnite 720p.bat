14142aab8261b8044eb2b5defc750c4f2cf48729
Brennon Overton <60564320+GeneBO98@users.noreply.github.com>
2023-10-21 16:28:34 -0500

    GameStreamLP configuration files

---
+++ GameStreamLP/GS Playnite 720p.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+start /MIN MultiMonitorTool.exe /Enable \\.\DISPLAY3
+timeout /t 1
+start /MIN MultiMonitorTool.exe /SetPrimary \\.\DISPLAY3
+timeout /t 1
+start /MIN gamestream_launchpad.exe 1280 720 gamestream_playnite.ini
+timeout /t 3
+powershell -ExecutionPolicy Bypass -File "Stop_Playnite.ps1"
+timeout /t 1
+powershell -ExecutionPolicy Bypass -File "SetPrimaryMon.ps1" 
---
+++ GameStreamLP/Unused/Stop_Playnite.ps1
@@ -0,0 +1,18 @@
+﻿$WindowTitle = "Calculator"
+
+#$SteamRunning = Get-Process | Where-Object { $_.ProcessName -like "steam" }
+#If ($SteamRunning.ProcessName -eq "steam") {
+#    Stop-Process -Name Steam
+#} else {
+#    continue
+#}
+
+$windowFound = $true
+while ($windowFound) {
+    Start-Sleep -Seconds 1
+    $windowFound = Get-Process | Where { $_.MainWindowTitle -eq $windowTitle }
+}
+Stop-Process -Name Playnite.FullscreenApp -Force
+Stop-Process -Name gamestream_launchpad -Force
+
+
