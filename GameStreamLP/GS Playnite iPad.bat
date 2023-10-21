913239aaf019c0629675b52ebf265c2c47359fa7
Brennon Overton <60564320+GeneBO98@users.noreply.github.com>
2023-10-21 17:41:27 -0500

    Update GS Playnite iPad.bat

--- GameStreamLP/GS Playnite iPad.bat
+++ GameStreamLP/GS Playnite iPad.bat
@@ -1,10 +1,2 @@
 @ECHO OFF
-start /MIN MultiMonitorTool.exe /Enable \\.\DISPLAY3
-timeout /t 1
-start /MIN MultiMonitorTool.exe /SetPrimary \\.\DISPLAY3
-timeout /t 1
 start /MIN gamestream_launchpad.exe 2048 1536 gamestream_playnite.ini
-timeout /t 3
-powershell -ExecutionPolicy Bypass -File "Stop_Playnite.ps1"
-timeout /t 1
-powershell -ExecutionPolicy Bypass -File "SetPrimaryMon.ps1" 
